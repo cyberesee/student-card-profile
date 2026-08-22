8d804daa8a7512e1031223909d8b96625f221d38
resee <wad@example.com>
2026-08-22 12:22:34 +0800

    Populated content & styles in card.js

--- card.js
+++ card.js
@@ -1,3 +1,10 @@
+//generate elements
 const name = document.createElement("h2");
 const desc = document.createElement("p");
 const avatar = document.createElement("img");
+
+//populate content & styles
+name.textContent = "Mary Therese Guevarra";
+desc.textContent = "BSIS2 student at La Verdad Christian College.";
+avatar.src = "assets/images/therese.png";
+avatar.alt = "Mary Therese Guevarra";
