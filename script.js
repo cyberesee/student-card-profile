e57712cb710a2441b29ddd38cd421a782b7bf75a
resee <wad@example.com>
2026-08-22 12:45:19 +0800

    Fixing the connection between style.css and script.js variables

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//script.js for card.js
+//script.js for card.js (first file name na nalagay)
 
 //generate elements
 const name = document.createElement("h2");
@@ -7,12 +7,18 @@ const avatar = document.createElement("img");
 
 //populate content & styles
 name.textContent = "Mary Therese Guevarra";
+name.className = "profile-name";
+
 desc.textContent = "BSIS2 student at La Verdad Christian College.";
+desc.className = "profile-bio";
+
 avatar.src = "assets/images/therese.png";
 avatar.alt = "Mary Therese Guevarra";
+avatar.className = "profile-avatar";
 
 //append profile card
 const profileContainer = document.getElementById("profile-container");
+profileContainer.classList.add("profile-card");
 profileContainer.appendChild(name);
 profileContainer.appendChild(desc);
 profileContainer.appendChild(avatar);
